5207241b55bcd8c59c17aa0290f775864e522d3b
adens123 <72215185+adens123@users.noreply.github.com>
2023-05-31 18:37:38 +0800

    update

--- js/all.js
+++ js/all.js
@@ -2,45 +2,26 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 // 漢堡menu導覽列
 const navCollapes = document.querySelector(".nav-collapes");
-// body下第一層全部div
-const divs = document.querySelectorAll("body > div");
 // 回到網頁最上面
 const backToTop = document.querySelector(".backToTop-btn");
 
-function render() {
-  // 將X圖示換成漢堡
-  mobileMenu.textContent = "menu";
-  // 隱藏漢堡導覽列
-  navCollapes.style.display = "none";
-  // 重新渲染body下第一層全部div
-  for (let i = 0; i < divs.length; i++) {
-    document.body.appendChild(divs[i]);
-  }
-}
-
 // 監聽漢堡menu的click事件
 mobileMenu.addEventListener("click", () => {
-  // 清空所有網頁內容
-  document.body.innerHTML = "";
-
-  // 判斷漢堡和x圖示去決定要渲染的div
   if (mobileMenu.textContent == "menu") {
-    // 渲染header和footer
     mobileMenu.textContent = "close";
     navCollapes.style.display = "flex";
-    document.body.appendChild(divs[0]);
-    document.body.appendChild(divs[divs.length - 1]);
   } else {
-    // 渲染所有網頁內容
-    render();
+    mobileMenu.textContent = "menu";
+    navCollapes.style.display = "none";
   }
 });
 
 // 監聽browser width
 window.addEventListener("resize", () => {
-  // 大於等於767px，mobile menu的圖示還是x時，渲染所有網頁內容
+  // 大於等於767px，mobile menu的圖示還是x時，關閉漢堡選單
   if (window.innerWidth >= 767 && mobileMenu.textContent == "close") {
-    render();
+    mobileMenu.textContent = "menu";
+    navCollapes.style.display = "none";
   }
 });
 
@@ -48,4 +29,36 @@ window.addEventListener("resize", () => {
 backToTop.addEventListener("click", (e) => {
   e.preventDefault();
   window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+// #price
+// -------------------------------------
+
+// 常見問題 all li
+const qaList = document.querySelectorAll(".qa-list .list-item");
+// show, hide icon
+const addRemoveIcon = document.querySelector(".qa-list .material-icons");
+// 常見問題答案的父層元素
+const qaContents = document.querySelectorAll(".qa-list .contents");
+
+// 取得常見問題答案的父層元素的高度，並將高度設為0
+window.addEventListener("DOMContentLoaded", () => {
+  qaContents.forEach((content) => {
+    content.dataset.height = content.offsetHeight;
+    content.style.height = "0";
+  });
+});
+
+qaList.forEach((item) => {
+  item.addEventListener("click", function (e) {
+    // 如果常見問題答案的父層高度是0，就改為dataset.height的高度
+    if (this.children[1].style.height == "0px") {
+      this.children[1].style.height = `${this.children[1].dataset.height}px`;
+      this.children[0].children[0].textContent = "remove";
+      // 如果常見問題答案的父層高度不是0，就改為0
+    } else {
+      this.children[1].style.height = "0";
+      this.children[0].children[0].textContent = "add";
+    }
+  });
 });
